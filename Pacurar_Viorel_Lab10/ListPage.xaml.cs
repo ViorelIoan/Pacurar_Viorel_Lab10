1f315e16f861f3844323411a96c9be9e0993dda4
Viorel Pacurar <viorelioan.pacurar@live.com>
2021-12-10 17:41:57 +0200

    Add project files.

---
+++ Pacurar_Viorel_Lab10/ListPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Nume_Pren_Lab10.Models;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Pacurar_Viorel_Lab10
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ListPage : ContentPage
+    {
+        public ListPage()
+        {
+            InitializeComponent();
+        }
+        async void OnSaveButtonClicked(object sender, EventArgs e)
+        {
+            var slist = (ShopList)BindingContext;
+            slist.Date = DateTime.UtcNow;
+            await App.Database.SaveShopListAsync(slist);
+            await Navigation.PopAsync();
+        }
+        async void OnDeleteButtonClicked(object sender, EventArgs e)
+        {
+            var slist = (ShopList)BindingContext;
+            await App.Database.DeleteShopListAsync(slist);
+            await Navigation.PopAsync();
+        }
+
+    }
+}
